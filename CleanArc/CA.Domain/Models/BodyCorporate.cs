661edf97a286dd454d21c547fc6eaafdf032f8e7
roblooney <robertml@telkomsa.net>
2020-02-11 18:31:16 +0200

    Added BodyCorporate and Unit to Domain

---
+++ CleanArc/CA.Domain/Models/BodyCorporate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA.Domain.Models
+{
+    class BodyCorporate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SSNumber { get; set; }
+        public string LotNumber { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2 { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public bool Active { get; set; }
+        public DateTime ActiveDate { get; set; }
+        public string MunicipalityAccount { get; set; }
+    }
+}
---
+++ CleanArc/CA.Domain/Models/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA.Domain.Models
+{
+    class Unit
+    {
+        public int Id { get; set; }
+        public int BcId { get; set; }
+        public string Name { get; set; }
+        public string WaterMeterNumber { get; set; }
+        public string ElectricityMeterNumber { get; set; }
+        public string GasMeterNumber { get; set; }
+        public string ERFNumber { get; set; }
+        public string SectionNumber { get; set; }
+        public string DoorNumber { get; set; }
+        public string FloorArea { get; set; }
+        public string ParticipationQuota { get; set; }
+    }
+}
